b60ae6571a168c403760cfdb348941546aec3be8
hisland <hisland@qq.com>
2019-07-12 11:10:02 +0800

    js 版 主程完成

---
+++ src/mine.js
@@ -0,0 +1,150 @@
+export default class Mine {
+  constructor(rows, cols, mineCount) {
+    this.rows = rows
+    this.cols = cols
+    this.max = rows * cols
+    this.mineCount = mineCount
+    this.cells = []
+    this.checkSet = new Set()
+    this.gameState = 'running'
+    this.init()
+  }
+  init() {
+    const { max } = this
+    const cells = []
+    for (let ii = 0; ii < max; ii++) {
+      cells.push({
+        value: 0,
+        mark: 'none',
+        opened: false,
+      })
+    }
+    const mines = this.randomMines()
+    for (const vv of mines) {
+      cells[vv].value = 9
+    }
+    this.cells = cells
+    this.cells.forEach((vv1, index1) => {
+      if (vv1.value !== 9) {
+        vv1.value = this.countMines(index1)
+      }
+    })
+    this.checkSet = new Set()
+  }
+  randomMines() {
+    const { max, mineCount } = this
+    const ss = new Set()
+    while (ss.size < mineCount) {
+      const rid = Math.floor(Math.random() * max)
+      ss.add(rid)
+    }
+    return ss
+  }
+  toIndex([xx, yy]) {
+    const { cols } = this
+    return cols * yy + xx
+  }
+  toPos(index) {
+    const { cols } = this
+    const xx = index % cols
+    const yy = (index - xx) / cols
+    return [xx, yy]
+  }
+  aroundIndex(index) {
+    const { rows, cols } = this
+    const [xx, yy] = this.toPos(index)
+    const arr1 = [
+      [xx - 1, yy - 1],
+      [xx, yy - 1],
+      [xx + 1, yy - 1],
+      [xx - 1, yy],
+      [xx + 1, yy],
+      [xx - 1, yy + 1],
+      [xx, yy + 1],
+      [xx + 1, yy + 1],
+    ]
+    const arr2 = arr1
+      .filter(([xx, yy]) => !(xx < 0 || xx >= cols || yy < 0 || yy >= rows))
+      .map(([xx, yy]) => cols * yy + xx)
+    return arr2
+  }
+  countMines(index) {
+    const indexList = this.aroundIndex(index)
+    const { cells } = this
+    let sum = 0
+    for (const index2 of indexList) {
+      if (cells[index2].value === 9) {
+        sum += 1
+      }
+    }
+    return sum
+  }
+  setMark(index) {
+    const { cells } = this
+    const one = cells[index]
+    if (one.mark === 'none') {
+      one.mark = 'flag'
+    } else if (one.mark === 'flag') {
+      one.mark = 'question'
+    } else if (one.mark === 'question') {
+      one.mark = 'none'
+    }
+    this.checkAll()
+  }
+  open(index) {
+    const { cells } = this
+    const one1 = cells[index]
+    if (one1.value === 9) {
+      // 失败, 雷
+      this.surrender()
+    } else if (one1.opened) {
+      // 已经打开
+    } else {
+      let deepCount1 = 0
+      let deepCount2 = 0
+      const { checkSet } = this
+      const deepCheck = index2 => {
+        deepCount1++
+        if (checkSet.has(index2)) return
+        checkSet.add(index2)
+        deepCount2++
+        if (deepCount2 > 300) {
+          return console.log('early out')
+        } else {
+          console.log('deepCheck', deepCount2)
+        }
+        const one2 = cells[index2]
+        if (!one2.opened) {
+          one2.opened = true
+          if (one2.value === 0) {
+            const indexList = this.aroundIndex(index2)
+            for (const index3 of indexList) {
+              deepCheck(index3)
+            }
+          }
+        }
+      }
+      deepCheck(index)
+      console.log(deepCount1)
+      this.checkAll()
+    }
+  }
+  checkAll() {
+    const pass = this.cells
+      .filter(vv => vv.value === 9)
+      .every(vv => vv.mark === 'flag')
+    if (pass) {
+      this.showAll()
+      this.gameState = 'accomplish'
+    }
+  }
+  showAll() {
+    for (const vv of this.cells) {
+      vv.opened = true
+    }
+  }
+  surrender() {
+    this.showAll()
+    this.gameState = 'fail'
+  }
+}
--- src/mine.vue
+++ src/mine.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="wrap">
     <div>
-      <button v-if="state.gameState==='ready'" @click="init()">开始</button>
-      <button v-if="state.gameState==='running'" @click="DoFail()">认输</button>
-      <button v-if="state.gameState==='accomplish'" @click="init()">再来</button>
-      <button v-if="state.gameState==='fail'" @click="init()">重来</button>
+      <button v-if="state.gameState==='running'" @click="DoFail()">认输☹️</button>
+      <button v-else-if="state.gameState==='accomplish'" @click="init()">再来😁</button>
+      <button v-else-if="state.gameState==='fail'" @click="init()">重来😁</button>
     </div>
     <div class="cellWrap" :style="{width:wrapWidth}">
       <template v-for="(vv0, index0) in state.cells">
         <div
           class="cell"
-          :class="vv0.flag"
+          :class="cellClz(vv0)"
           :key="index0"
-          @click="DoOpen(vv0, index0)"
-          @contextmenu="DoFlag(vv0, $event)"
+          @click="DoOpen(index0)"
+          @contextmenu="DoFlag(index0, $event)"
         >{{text(vv0)}}</div>
       </template>
     </div>
   </div>
 </template>
 <script>
+import GameCore from './mine.js'
 export default {
   components: {},
   data() {
@@ -27,12 +27,9 @@ export default {
       config: {
         rows: 10,
         cols: 20,
-        mines: 50,
+        mines: 3,
       },
-      state: {
-        gameState: 'ready',
-        cells: [],
-      },
+      state: {},
     }
   },
   watch: {},
@@ -41,162 +38,52 @@ export default {
     wrapWidth() {
       return this.config.cols * 20 + 'px'
     },
-    max() {
-      return this.config.rows * this.config.cols
-    },
   },
   methods: {
-    init() {
-      const len = this.max
-      const cells = []
-      for (let ii = 0; ii < len; ii++) {
-        cells.push({
-          value: -1,
-          flag: 'normal',
-        })
-      }
-      const mines = this.randomMines()
-      for (const vv of mines) {
-        cells[vv].value = 9
-      }
-      this.state.cells = cells
-      cells.forEach((vv1, index1) => {
-        if (vv1.value !== 9) {
-          vv1.value = this.countMines(this.aroundPos(index1))
+    cellClz(vv0) {
+      if (vv0.opened) {
+        if (vv0.mark === 'flag') {
+          if (vv0.value !== 9) {
+            return 'error'
+          } else {
+            return 'right'
+          }
+        } else {
+          return 'opened'
         }
-      })
-      this.state.gameState = 'running'
-    },
-    randomMines() {
-      const rid = () => Math.floor(Math.random() * this.max)
-      const ss = new Set()
-      while (ss.size < this.config.mines) {
-        ss.add(rid())
       }
-      return ss
+    },
+    init() {
+      this.state = new GameCore(
+        this.config.rows,
+        this.config.cols,
+        this.config.mines
+      )
     },
     text(vv0) {
-      if (vv0.flag === 'normal') {
-        return ''
-      } else if (vv0.flag === 'flag') {
-        return 'F'
-      } else if (vv0.flag === 'question') {
-        return '?'
-      } else if (vv0.flag === 'open') {
+      if (vv0.opened) {
         if (vv0.value === 9) {
           return '💣'
         } else {
           return vv0.value || ''
         }
-      }
-    },
-    toIndex([xx, yy]) {
-      return this.config.cols * yy + xx
-    },
-    toPos(index) {
-      const xx = index % this.config.cols
-      const yy = (index - xx) / this.config.cols
-      return [xx, yy]
-    },
-    aroundIndex(index) {
-      const { cols } = this.config
-      const arr = [
-        index - cols - 1,
-        index - cols,
-        index - cols + 1,
-        index - 1,
-        index + 1,
-        index + cols - 1,
-        index + cols,
-        index + cols + 1,
-      ]
-      const { max } = this
-      return arr.filter(vv => vv >= 0 && vv < max)
-    },
-    aroundPos(index) {
-      const [xx, yy] = this.toPos(index)
-      const arr = [
-        [xx - 1, yy - 1],
-        [xx, yy - 1],
-        [xx + 1, yy - 1],
-        [xx - 1, yy],
-        [xx + 1, yy],
-        [xx - 1, yy + 1],
-        [xx, yy + 1],
-        [xx + 1, yy + 1],
-      ]
-      const maxX = this.config.cols - 1
-      const maxY = this.config.rows - 1
-      return arr.filter(
-        ([xx, yy]) => !(xx < 0 || yy < 0 || xx > maxX || yy > maxY)
-      )
-    },
-    countMines2(indexList) {
-      const { cells } = this.state
-      let sum = 0
-      for (const pos of indexList) {
-        if (cells[this.toIndex(pos)].value === 9) {
-          sum += 1
+      } else {
+        if (vv0.mark === 'flag') {
+          return '🚩'
+        } else if (vv0.mark === 'question') {
+          return '❔'
         }
       }
-      return sum
-    },
-    countMines(posList) {
-      const { cells } = this.state
-      let sum = 0
-      for (const pos of posList) {
-        if (cells[this.toIndex(pos)].value === 9) {
-          sum += 1
-        }
-      }
-      return sum
     },
     DoFail() {
-      for (const vv of this.state.cells) {
-        vv.flag = 'open'
-      }
-      this.state.gameState = 'ready'
+      this.state.surrender()
     },
-    DoOpen(vv0, index) {
-      if (vv0.value === 9) {
-        // 失败, 雷
-        // } else if (vv0.value > -1) {
-        // 已经打开
-      } else {
-        const { cells } = this.state
-        let deepCount = 0
-        const doneSet = new Set()
-        const deepIn = index1 => {
-          deepCount++
-          if (deepCount > 100) {
-            console.log('early out')
-            return
-          }
-          if (!doneSet.has(index1)) {
-            doneSet.add(index1)
-            const vv1 = cells[index1]
-            vv1.flag = 'open'
-            if (!vv1.value) {
-              const posList = this.aroundPos(index1)
-              for (const pos2 of posList) {
-                const index2 = this.toIndex(pos2)
-                deepIn(index2)
-              }
-            }
-          }
-        }
-        deepIn(index)
-      }
+    DoOpen(index) {
+      this.state.open(index)
     },
-    DoFlag(vv0, ee) {
+    DoFlag(index, ee) {
       ee.preventDefault()
-      if (vv0.flag === 'normal') {
-        vv0.flag = 'flag'
-      } else if (vv0.flag === 'flag') {
-        vv0.flag = 'question'
-      } else if (vv0.flag === 'question') {
-        vv0.flag = 'normal'
-      }
+      this.state.setMark(index)
     },
   },
   mounted() {
@@ -218,16 +105,14 @@ export default {
   box-sizing: border-box;
   cursor: default;
   user-select: none;
-  &.normal {
-    background-color: #999;
+  background-color: #999;
+  &.right {
+    background-color: green;
   }
-  &.flag {
-    background-color: #999;
+  &.error {
+    background-color: red;
   }
-  &.question {
-    background-color: #999;
-  }
-  &.open {
+  &.opened {
     background-color: #fff;
   }
 }

ada92e53f38fd14c213787aba3f854b09448be77
hisland <hisland@qq.com>
2021-07-23 14:33:16 +0800

    misc update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import dirListPlugin from './vite2-plugin-dir-list.js'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dirListPlugin()],
+  server: {
+    host: '0.0.0.0',
+  },
   optimizeDeps: {
     entries: [],
   },

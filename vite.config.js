6469ea4c19d049100b1fd81391c3e330a0023657
hisland <hisland@qq.com>
2021-09-25 23:09:20 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dirListPlugin from './vite2-plugin-dir-list.js'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,14 @@ export default defineConfig({
   },
   optimizeDeps: {
     entries: [],
+  },
+  root: resolve(__dirname, 'src/flash-dot'),
+  base: './',
+  build: {
+    // rollupOptions: {
+    //   input: {
+    //     // 'mine-sweeper': resolve(__dirname, 'src/mine-sweeper/index.html'),
+    //   },
+    // },
   },
 })
